a80b0aa3c69cc715e86c805113e62601718618d5
wenogk <drf325@nyu.edu>
2020-06-09 13:50:43 +0400

    story title

--- js/script.js
+++ js/script.js
@@ -2,5 +2,11 @@
 //gsap.to(".language1", {duration: 1, marginRight:"-200px"});
 //gsap.to(".language2", {duration: 1});
 setTimeout(() => {
-gsap.to(".languageButton", {duration: 3, opacity:1});
+gsap.to(".firstHeader", {duration: 3, opacity:1});
 }, 1000);
+setTimeout(() => {
+gsap.to(".firstHeader", {duration: 2, opacity:0});
+}, 4000);
+setTimeout(() => {
+gsap.to(".languageButton", {duration: 3, opacity:1});
+}, 6000);
