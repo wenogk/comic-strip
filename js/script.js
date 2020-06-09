216cad405eff341af8349119ec589924367fb65e
wenogk <drf325@nyu.edu>
2020-06-09 12:54:46 +0400

    sample colors

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
 
 //gsap.to(".language1", {duration: 1, marginRight:"-200px"});
 //gsap.to(".language2", {duration: 1});
-//gsap.to(".language3", {duration: 1});
+gsap.to(".languageButton", {duration: 3, opacity:1});
