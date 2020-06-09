b09d31300da7267adeefb68db83f8bb3c3407d02
wenogk <drf325@nyu.edu>
2020-06-09 17:31:13 +0400

    story.js

---
+++ js/story.js
@@ -0,0 +1,30 @@
+let currentPath = 0;
+let prevPath = 0;
+
+let PATHS = {
+  1: {
+    image: ""
+  },
+  2: {
+    image: ""
+  },
+  3: {
+    image: ""
+  },
+  4: {
+    image: ""
+  }
+  "5a": {
+    image: ""
+  },
+  "5b": {
+    image: ""
+  }
+  6 : {
+    image: ""
+  }
+}
+
+function loadPath() {
+  
+}
