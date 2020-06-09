c616b79f82df22d60dbe01103977cfdc8479b531
wenogk <drf325@nyu.edu>
2020-06-09 19:03:04 +0400

    options in story logic

--- js/story.js
+++ js/story.js
@@ -29,23 +29,30 @@ function loadPath(id) {
   } else if(id == 4) {
     $(".nextButton").hide()
 
-    return "<a onclick='nextPath()'" + ">OPTION 1</a><br />" + "<a onclick='nextPath()'" + ">OPTION 2</a>"
+    return "<a onclick='nextPath(5)'" + ">OPTION 1</a><br />" + "<a onclick='nextPath(6)'" + ">OPTION 2</a>"
   } else if(id == 5) {
+    $(".nextButton").show()
     return "five"
   } else if(id == 6) {
+    $(".nextButton").show()
     return "six"
   } else if(id == 7) {
     return "seven"
   }
 }
 
-function nextPath() {
+function nextPath(id=null) {
   if(currentPath==7) {
     box();
     $(".nextButton").hide()
     $(".backButton").hide()
   } else {
-    currentPath+=1;
+    prevPath = currentPath;
+    if(id==null) {
+      currentPath+=1;
+    } else {
+      currentPath = id
+    }
     let element = "<div style='background:" + "#"+((1<<24)*Math.random()|0).toString(16) +"' class='path_" + currentPath + " newStory' >" + loadPath(currentPath)  + "</div>"
     $("body").append(element);
     gsap.to(".path_" + currentPath, {duration: 1, left:0}); // after this delete the previous path element
@@ -59,7 +66,9 @@ function previousPath() {
     //alert("remove all")
 
   } else {
-    currentPath-=1;
+    let tempPath = prevPath;
+    prevPath = currentPath;
+    currentPath = tempPath;
   //  alert(currentPath)
     let element = "<div style='background:" + "#"+((1<<24)*Math.random()|0).toString(16) +"' class='path_" + currentPath + " prevStory' >" + loadPath(currentPath)  + "</div>"
     $("body").append(element);
