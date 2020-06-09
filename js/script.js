d4862f5a468791db4dd1aa8dc7fa77667157bcc1
wenogk <drf325@nyu.edu>
2020-06-09 11:39:15 +0400

    languages

--- js/script.js
+++ js/script.js
@@ -1,1 +1,4 @@
-gsap.to(".language2", {duration: 1, x: 100, marginLeft:500});
+gsap.to(".language1", {duration: 1, x: 100, marginRight:-200});
+gsap.to(".language2", {duration: 1, x: 100, marginLeft:-300});
+gsap.to(".language3", {duration: 1, x: 100, marginRight:0});
+gsap.to(".language4", {duration: 1, x: 100, marginLeft:-300});
