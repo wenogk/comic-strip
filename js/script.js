ac0ae0d28ddc44c0013530326955f2bb9519cdec
wenogk <drf325@nyu.edu>
2020-06-09 12:44:51 +0400

    working styles

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-gsap.to(".language1", {duration: 1, x: 100, marginRight:-200});
-gsap.to(".language2", {duration: 1, x: 100, marginLeft:-300});
-gsap.to(".language3", {duration: 1, x: 100, marginRight:0});
-gsap.to(".language4", {duration: 1, x: 100, marginLeft:-300});
+
+//gsap.to(".language1", {duration: 1, marginRight:"-200px"});
+//gsap.to(".language2", {duration: 1});
+//gsap.to(".language3", {duration: 1});
