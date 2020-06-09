8aa89b8ace837253b7ebeb4cf0bdfbdd1a769871
wenogk <drf325@nyu.edu>
2020-06-09 12:59:31 +0400

    delay

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 
 //gsap.to(".language1", {duration: 1, marginRight:"-200px"});
 //gsap.to(".language2", {duration: 1});
+setTimeout(() => {
 gsap.to(".languageButton", {duration: 3, opacity:1});
+}, 1000);
