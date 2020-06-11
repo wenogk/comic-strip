ce177567f57565ab53b3514ffe5df8316c036777
wenogk <drf325@nyu.edu>
2020-06-11 16:37:11 +0400

    script.js commented

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 
 //gsap.to(".language1", {duration: 1, marginRight:"-200px"});
 //gsap.to(".language2", {duration: 1});
-let boxState = "boxFullyHide";
-$( window ).resize(function() {
+let boxState = "boxFullyHide"; //initial state of box (the next and restart controls + about section) is fully hidden
+$( window ).resize(function() { // on resize of the window animate the box back to wherever its supposed to be
   console.log(boxState)
   switch(boxState) {
     case "boxFullyHide":
@@ -20,28 +20,28 @@ $( window ).resize(function() {
   }
 });
 
-setTimeout(() => {
+setTimeout(() => { //initial animation of fade in of the logo + hiding of the "Loading.. " text
 $(".loader").hide();
 gsap.to(".titleSvg", {duration: 3, opacity:1});
 }, 5000);
 
-setTimeout(() => {
+setTimeout(() => { // animation move and resize the logo to the very left top
 gsap.to(".titleSvg", {duration: 1, width: "15%", left: "0px", top: "0px", opacity:1});
 }, 8000);
 
-setTimeout(() => {
+setTimeout(() => { // entering animation of the language "droplet" buttons
 gsap.to(".language1", {duration: 1, top: "50%", opacity:1});
 gsap.to(".language2", {duration: 2, top: "50%", opacity:1});
 gsap.to(".language3", {duration: 3, top: "50%", opacity:1});
 }, 9000);
 
-function getLanguageDropsBackToFirstState() {
+function getLanguageDropsBackToFirstState() {  // animation of the language "droplet" buttons to go to initial state
   gsap.to(".language1", {duration: 1, top: "48%", opacity:1, display: "block"});
   gsap.to(".language2", {duration: 1, top: "48%", opacity:1, display: "block"});
   gsap.to(".language3", {duration: 1, top: "48%", opacity:1, display: "block"});
 }
 
-function hideLanguageDrops() {
+function hideLanguageDrops() { // leaving animation of the language "droplet" buttons
   setTimeout(() => {
     gsap.to(".language1", {duration: 1,  opacity:0, display: "none"});
     gsap.to(".language2", {duration: 1, opacity:0, display: "none"});
@@ -49,28 +49,14 @@ function hideLanguageDrops() {
     }, 500);
 }
 
-function boxFullyHide(s = 2) {
+function boxFullyHide(s = 2) { // leaving animation of the controls box
   boxState = "boxFullyHide";
   let h = window.innerHeight - 50;
   gsap.to(".boxContainer", {duration: s, display: "none", opacity:0, top:h, width:"50%", left:"25%"});
 }
 
-function boxControlsOnly(s=2) {
+function boxControlsOnly(s=2) { // animation of the control box to show only controls
   boxState = "boxControlsOnly"
   let h = window.innerHeight - 50;
   gsap.to(".boxContainer", {duration: s, display: "block", opacity:1, top:h, width:"10%", left:"45%"});
 }
-
-function boxBack(s=2) {
-  gsap.to(".boxContainer", {duration: s, display:"block", opacity:1, top:100, width:"60%", left:"20%"});
-}
-function showCheek() {
-   $("#cheeks").css("opacity","1");
-   console.log("hovered");
-
-  }
-function hideCheek() {
-   $("#cheeks").css("opacity","0");
-   console.log("not hovered");
-
-  }
