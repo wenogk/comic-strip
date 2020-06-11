a779981338be6106a3f7ff092b3b6be944c4ef36
steeti <sht318@nyu.edu>
2020-06-11 16:38:11 +0400

    Interactivity Comments

--- js/interactivity.js
+++ js/interactivity.js
@@ -4,16 +4,21 @@ var showeraudio = $("#shower")[0];
 var typingaudio = $("#typing")[0];
 var dropaudio = $("#drop")[0];
 var clockaudio = $("#clock")[0];
-
+//first image of the comic,
+//When the cursor enters the area then we play the audio, then we display the box button, otherwise its hidden by default
+//We are basiclly telling the cursor that when you enter the body of this class then play the following audio
 $("body").on('mouseenter', '.path_1 > div > img', function(){
   boxControlsOnly(1);
   ouchaudio.play();
 });
+//when the cursor leaves the body of the class then we pause the audio, and restart it by setting the current time for to 0
 $("body").on('mouseout', '.path_1 > div > img', function(){
   ouchaudio.pause();
   ouchaudio.currentTime = 0;
 
 });
+
+//Same Concept as above, just different audio and pic
 $("body").on('mouseenter', '.path_2b > div > img', function(){
   boxControlsOnly(1);
   dropaudio.play();
@@ -23,6 +28,7 @@ $("body").on('mouseout', '.path_2b > div > img', function(){
   dropaudio.currentTime = 0;
 });
 
+//Same concept different pic and audio
 $("body").on('mouseenter', '.path_2c > div > img', function(){
   boxControlsOnly(1);
   panicaudio.play();
@@ -31,7 +37,7 @@ $("body").on('mouseout', '.path_2c > div > img', function(){
   panicaudio.pause();
   panicaudio.currentTime = 0;
 });
-
+//Same concept as above, different pic and audio, also two sounds on the same picture over each other(sounded cooler :) )
 $("body").on('mouseenter', '.path_2b > div > img', function(){
   boxControlsOnly(1);
   panicaudio.play();
@@ -40,7 +46,7 @@ $("body").on('mouseout', '.path_2b > div > img', function(){
   panicaudio.pause();
   panicaudio.currentTime = 0;
 });
-
+//same concept, differnt pic and audio
 $("body").on('mouseenter', '.path_3 > div > img', function(){
   boxControlsOnly(1);
   dropaudio.play();
@@ -49,7 +55,7 @@ $("body").on('mouseout', '.path_3 > div > img', function(){
   dropaudio.pause();
   dropaudio.currentTime = 0;
 });
-
+//same concept, differnt pic and audio
 $("body").on('mouseenter', '.path_5 > div > img', function(){
   boxControlsOnly(1);
   showeraudio.play();
@@ -58,6 +64,7 @@ $("body").on('mouseout', '.path_5 > div > img', function(){
   showeraudio.pause();
   showeraudio.currentTime = 0;
 });
+//same concept, differnt pic and audio
 $("body").on('mouseenter', '.path_7 > div > img', function(){
   boxControlsOnly(1);
   clockaudio.play();
@@ -66,6 +73,7 @@ $("body").on('mouseout', '.path_7 > div > img', function(){
   clockaudio.pause();
   clockaudio.currentTime = 0;
 });
+//same concept, differnt pic and audio
 $("body").on('mouseenter', '.path_6 > div > img', function(){
   boxControlsOnly(1);
   typingaudio.play();
@@ -74,3 +82,5 @@ $("body").on('mouseout', '.path_6 > div > img', function(){
   typingaudio.pause();
   typingaudio.currentTime = 0;
 });
+
+//The last panel there was no music so the box button was showing by default.
